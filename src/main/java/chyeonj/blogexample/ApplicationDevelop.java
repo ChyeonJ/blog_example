750cdc3699bd4b2c633af6f87d32b961fdae89e8
ChyeonJ <akdntm4757@gmail.com>
2025-11-17 17:29:02 +0900

    save

---
+++ src/main/java/chyeonj/blogexample/ApplicationDevelop.java
@@ -0,0 +1,11 @@
+package chyeonj.blogexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "chyeonj.blogexample")
+public class ApplicationDevelop {
+
+    public static void main(String[] args){ SpringApplication.run(ApplicationDevelop.class, args);}
+
+}
