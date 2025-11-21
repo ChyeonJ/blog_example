ff2f8375fc20446636bc6de9e6025e1dd1853aa0
ChyeonJ <akdntm4757@gmail.com>
2025-11-22 00:48:58 +0900

    update(modify)

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -14,51 +14,28 @@ if (deleteButton) {
    });
 }
 
-수정 기능
-//const modifyButton = document.getElementById('modify-btn');
-//
-//if (modifyButton){
-//   modifyButton.addEventListener('click', event => {
-//    let params = new URLSearchParams(location.search);
-//    let id = params.get('id');
-//
-//    fetch(`/api/articles/${id}`, {
-//        method: 'PUT',
-//        headers: {
-//            "Content-Type" : "application/json",
-//        },
-//        body: JSON.stringify({
-//            title: document.getElementById('title').value,
-//            content: document.getElementById('content').value
-//        })
-//     })
-//     .then(() => {
-//        alert('수정완료');
-//        location.replace(`/articles/${id}`);
-//     });
-//   });
-//}
+//수정 기능
 const modifyButton = document.getElementById('modify-btn');
 
-if (modifyButton) {
+if (modifyButton){
     modifyButton.addEventListener('click', event => {
+    let params = new URLSearchParams(location.search);
+    let id = params.get('id');
 
-        // hidden input에서 id 읽기
-        let id = document.getElementById('article-id').value;
-
-        fetch(`/api/articles/${id}`, {
-            method: 'PUT',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                title: document.getElementById('title').value,
-                content: document.getElementById('content').value
-            })
+    fetch(`/api/articles/${id}`, {
+        method: 'PUT',
+        headers: {
+            "Content-Type" : "application/json",
+        },
+        body: JSON.stringify({
+            title: document.getElementById('title').value,
+            content: document.getElementById('content').value
         })
-        .then(() => {
-            alert('수정완료');
-            location.replace(`/articles/${id}`);
-        });
+     })
+     .then(() => {
+        alert('수정완료');
+        location.replace(`/articles/${id}`);
+     });
     });
+
 }
