e6014cc74535967187c8b4a135fa4d265cc1d51e
ChyeonJ <akdntm4757@gmail.com>
2025-11-22 00:39:37 +0900

    sss

--- src/main/resources/static/js/article.js
+++ src/main/resources/static/js/article.js
@@ -15,46 +15,50 @@ if (deleteButton) {
 }
 
 수정 기능
+//const modifyButton = document.getElementById('modify-btn');
+//
+//if (modifyButton){
+//   modifyButton.addEventListener('click', event => {
+//    let params = new URLSearchParams(location.search);
+//    let id = params.get('id');
+//
+//    fetch(`/api/articles/${id}`, {
+//        method: 'PUT',
+//        headers: {
+//            "Content-Type" : "application/json",
+//        },
+//        body: JSON.stringify({
+//            title: document.getElementById('title').value,
+//            content: document.getElementById('content').value
+//        })
+//     })
+//     .then(() => {
+//        alert('수정완료');
+//        location.replace(`/articles/${id}`);
+//     });
+//   });
+//}
 const modifyButton = document.getElementById('modify-btn');
 
-if (modifyButton){
+if (modifyButton) {
     modifyButton.addEventListener('click', event => {
-    let params = new URLSearchParams(location.search);
-    let id = params.get('id');
 
-    fetch(`/api/articles/${id}`, {
-        method: 'PUT'
-        headers: {
-            "Content-Type" : "application/json",
-        },
-        body: JSON.stringify({
-            title: document.getElementById('title').value;
-            content: document.getElementById('content').value;
+        // hidden input에서 id 읽기
+        let id = document.getElementById('article-id').value;
+
+        fetch(`/api/articles/${id}`, {
+            method: 'PUT',
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                title: document.getElementById('title').value,
+                content: document.getElementById('content').value
+            })
         })
-     })
-     .then(() => {
-        alert('수정완료');
-        location.replace(`/articles/${id}`);
-     });
+        .then(() => {
+            alert('수정완료');
+            location.replace(`/articles/${id}`);
+        });
     });
-//const modifyButton = document.getElementById('modify-btn');
-
-//if (modifyButton){
-//    modifyButton.addEventListener('click', () => {
-//        let id = document.getElementById('article-id').value;
-//        fetch(`/api/articles/${id}`, {
-//            method: 'PUT',
-//            headers: { "Content-Type" : "application/json" },
-//            body: JSON.stringify({
-//                title: document.getElementById('title').value,
-//                content: document.getElementById('content').value
-//            })
-//        })
-//        .then(() => {
-//            alert('수정 완료');
-//            location.replace(`/articles/${id}`);
-//        })
-//        .catch(err => console.error(err));
-//    });
-//}
 }
