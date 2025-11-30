763003b536715aed5be51b80ab37990d01f3e865
ChyeonJ <akdntm4757@gmail.com>
2025-11-30 19:59:25 +0900

    1

---
+++ src/main/java/chyeonj/blogexample/Service/RefreshTokenService.java
@@ -0,0 +1,18 @@
+package chyeonj.blogexample.Service;
+
+import chyeonj.blogexample.blogexam.domain.RefreshToken;
+import chyeonj.blogexample.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+
+}
