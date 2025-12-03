4a83986ec597f7cc3859975fc12cbfd11c6f12c4
ChyeonJ <akdntm4757@gmail.com>
2025-12-03 21:50:05 +0900

    Update JwtFactory.java

--- src/main/java/chyeonj/blogexample/config/jwt/JwtFactory.java
+++ src/main/java/chyeonj/blogexample/config/jwt/JwtFactory.java
@@ -17,14 +17,14 @@ public class JwtFactory {
 
     private String subject = "test@email.com";
     private Date issuedAt = new Date();
-    private Date expriation = new Date(new Date().getTime() + Duration.ofDays(14).toMillis());
+    private Date expiration = new Date(new Date().getTime() + Duration.ofDays(14).toMillis();
     private Map<String, Object> clamis = emptyMap();
 
     @Builder
     public JwtFactory(String subject, Date issuedAt, Date expriation, Map<String, Object> clamis){
         this.subject = subject != null ? subject : this.subject;
         this.issuedAt = issuedAt != null ? issuedAt : this.issuedAt;
-        this.expriation = expriation != null ? expriation : this.issuedAt;
+        this.expiration = expriation != null ? expriation : this.issuedAt;
         this.clamis = clamis != null ? clamis : this.clamis;
     }
 
@@ -38,7 +38,7 @@ public class JwtFactory {
                 .setHeaderParam(Header.TYPE, Header.JWT_TYPE)
                 .setIssuer(jwtProperties.getIssuer())
                 .setIssuedAt(issuedAt)
-                .setExpiration(expriation)
+                .setExpiration(expiration)
                 .addClaims(clamis)
                 .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
                 .compact();
